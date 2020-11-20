ac5b636d322ceff410b2a184c3f0632f62f04c64
GustavoGatti <70723720+GustavoGatti@users.noreply.github.com>
2020-11-19 21:42:35 -0300

    Modulos de perfil e feed

--- lib/screens/Feed.dart
+++ lib/screens/Feed.dart
@@ -4,33 +4,46 @@ import 'package:projeto_muh_compmov/models/user_model.dart';
 import 'package:projeto_muh_compmov/screens/Foto.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-Foto1(UserModel model){
+class FeedPerfil extends StatefulWidget {
 
-  for(int i=0; i< model.preco.length; i++){
-    Foto(model.preco[i], model.descricao[i], model.imagem[i]);
-  }
+  final UserModel model;
 
+  FeedPerfil(this.model);
+
+  @override
+  _FeedPerfil createState() => _FeedPerfil(this.model);
 }
 
-class FeedPerfil extends StatelessWidget {
+
+
+class _FeedPerfil extends State<FeedPerfil> {
+
+  final UserModel model;
+
+  _FeedPerfil(this.model);
+
   @override
   Widget build(BuildContext context) {
 
-    return ScopedModelDescendant<UserModel>(
-        builder: (context, child, model){
-          return  Container(
-            child: Column(
-              children: [
-                Wrap(
-                  children: [
-            Foto(model.preco[0], model.descricao[0], model.imagem[0])
-                  ],
-                )
-              ],
-            )
-          );
-        }
+    return  Container(
+      child: Column(
+        children: [
+          Wrap(
+            children: [
+              Foto(model.preco[0], model.descricao[0], model.imagem[0]),
+              Foto(model.preco[1], model.descricao[1], model.imagem[1]),
+              Foto(model.preco[2], model.descricao[2], model.imagem[2]),
+              Foto(model.preco[1], model.descricao[1], model.imagem[1]),
+              Foto(model.preco[2], model.descricao[2], model.imagem[2]),
+              Foto(model.preco[1], model.descricao[1], model.imagem[1]),
+              Foto(model.preco[2], model.descricao[2], model.imagem[2]),
+              Foto(model.preco[0], model.descricao[0], model.imagem[0])
+            ],
+          )
+        ],
+      ),
     );
+
 
   }
 }
